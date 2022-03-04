4a2332b8c0e81d1303062a80d7ccc6de17ba0e7e
Rushikesh Suryawanshi <rsuryawanshi@2020ai.ai>
2022-03-04 16:22:47 +0530

    Spring boot Heroku

---
+++ src/test/java/com/example/heroku/Demo/HerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heroku.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
